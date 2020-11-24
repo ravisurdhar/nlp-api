0f88d6e9ab7ec9b2a3116265407e385bb79cf6db
raviscoop <ravi@takescoop.com>
2020-11-24 09:33:37 -1000

    Fix lint, adds GH workflow

--- flaskr/api.py
+++ flaskr/api.py
@@ -2,35 +2,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    data = {'text': 'App Works!'}
     return {
-    'status': 'success',
-    'code': 200,
-    'data': {
-    	'text': 'App Works!'
-    	}
+        'status': 'success',
+        'code': 200,
+        'data': data
     }
+
 
 @app.route('/tag/<text>')
 def tagger(text):
+    data = {'text': text}
     return {
-    'status': 'success',
-    'code': 200,
-    'data': {
-    	'text': text
-    	}
+            'status': 'success',
+            'code': 200,
+            'data': data
     }
+
 
 @app.errorhandler(404)
 def page_not_found(error):
+    message = {'text': 'Page Not Found'}
     return {
-    'status': 'error',
-    'code': 404,
-    'message': {
-    	'text': 'Page Not Found'
-    	}
+           'status': 'error',
+           'code': 404,
+           'message': message
     }, 404
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
